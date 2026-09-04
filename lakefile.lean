215ac9bd21a696fe96d5aa80886473999a3f89e9
Christian Pehle <christian.pehle@gmail.com>
2026-09-04 15:02:24 -0400

    chore: prepare public Apache release

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,8 @@ open Lake DSL
 
 package «LeanUrdfTypeProvider»
 
-require LeanTest from "/Users/pehle/dev/lean_test"
+require LeanTest from git "https://github.com/cpehle/lean_test.git" @
+  "b42cd3d78716e5a2de5b640ac82d7fe3f05f2a4c"
 
 @[default_target]
 lean_lib LeanUrdfTypeProvider where
